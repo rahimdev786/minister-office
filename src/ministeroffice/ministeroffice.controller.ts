d6a830fdc983cfe4d69b4b86facb8b83b2d70870
diniljosemoi <diniljose@moi.gov.kw>
2024-06-04 17:59:40 +0300

    get left

--- src/ministeroffice/ministeroffice.controller.ts
+++ src/ministeroffice/ministeroffice.controller.ts
@@ -16,7 +16,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 @Controller('MinisterOffice')
 export class MinistryOfficeController {
-  constructor(private readonly ministryOfficeService: MinistryOfficeService) {}
+  constructor(private readonly ministryOfficeService: MinistryOfficeService) { }
 
   @Post('Register')
   async createMinisterUser(
@@ -24,7 +24,7 @@ export class MinistryOfficeController {
     @Res() res: FastifyReply,
   ): Promise<OwnerDetails> {
     try {
-      const record = this.ministryOfficeService.saveOwnerDetails(
+      const record = this.ministryOfficeService.saveOwnerDetails2(
         createMinistryOfficeDto,
       );
       if (!record) {
@@ -47,14 +47,14 @@ export class MinistryOfficeController {
 
 
 
-@Post(':ownerCivilIdNumber/addRelation')
+  @Post(':ownerCivilIdNumber/addRelation')
   addRelation(
     @Param('ownerCivilIdNumber') ownerCivilIdNumber: string,
     @Body() updateRelationDto: OwnerRelationsDTO,
-) {
-  
-  console.log(ownerCivilIdNumber)
-       return this.ministryOfficeService.addRelation(ownerCivilIdNumber, updateRelationDto);
+  ) {
+
+    console.log(ownerCivilIdNumber)
+    return this.ministryOfficeService.addRelation(ownerCivilIdNumber, updateRelationDto);
   }
 
 
@@ -66,6 +66,32 @@ export class MinistryOfficeController {
     return this.ministryOfficeService.updateOwner(ownerCivilIdNumber, updateUserDto);
   }
 
+  @Get('getOwnerDetails/:ownerCivilIdNumber')
+  async getOwnerDetails(
+    @Param('ownerCivilIdNumber') ownerCivilIdNumber: string,
+    @Res() res: FastifyReply,
+  ): Promise<OwnerDetails> {
+    try {
+      const ownerDetails = await this.ministryOfficeService.getOwnerDetails(ownerCivilIdNumber);
+      if (!ownerDetails) {
+        return res.status(HttpStatus.NOT_FOUND).send({
+          status: HttpStatus.NOT_FOUND,
+          message: 'Owner details not found',
+        });
+      }
+      return res.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        data: ownerDetails,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).send({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Failed to fetch owner details',
+      });
+    }
+  }
+
+
   @Get('Findall/:civilId')
   async getAllNotesByCivilID(
     @Param('civilId') civilId: string,
@@ -73,24 +99,24 @@ export class MinistryOfficeController {
     @Query('limit') limit: number = 10,
     @Res() res: FastifyReply) {
     try {
-      const {currentpage,totalpages,
-        totalItems, data } = await this.ministryOfficeService.findbyNoteCivilId(civilId,page, limit);
+      const { currentpage, totalpages,
+        totalItems, data } = await this.ministryOfficeService.findbyNoteCivilId(civilId, page, limit);
       if (currentpage > totalpages) {
         return res.send({
-          data:data,
-          status:400,
+          data: data,
+          status: 400,
           message: 'no data avaiable on this page'
         });
-      } 
+      }
       if (data.length === 0) {
         return res.send({
-          data:data,
+          data: data,
           status: HttpStatus.NOT_FOUND,
-          message: 'data not found with page = ' +currentpage+ ' on ' + civilId,
+          message: 'data not found with page = ' + currentpage + ' on ' + civilId,
         });
       }
       return res.send({
-         currentpage, totalpages,
+        currentpage, totalpages,
         totalItems, data
       });
     } catch (error) {
@@ -101,24 +127,24 @@ export class MinistryOfficeController {
     }
   }
 
-@Get('Findall')
-async getAllNotes(
-  @Query('page') page: number = 1,
-  @Query('limit') limit: number = 10,
-  @Res() res: FastifyReply) {
+  @Get('Findall')
+  async getAllNotes(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Res() res: FastifyReply) {
     try {
-      const {currentpage,totalpages,
+      const { currentpage, totalpages,
         totalItems, data } = await this.ministryOfficeService.findbyAllNotes(page, limit);
       if (currentpage > totalpages) {
         return res.send({
-          data:data,
-          status:400,
+          data: data,
+          status: 400,
           message: 'no data avaiable on this page ' + currentpage
         });
-      } 
+      }
       if (data.length === 0) {
         return res.send({
-          data:data,
+          data: data,
           status: HttpStatus.NOT_FOUND,
           message: 'data not found',
         });
@@ -130,7 +156,7 @@ async getAllNotes(
     } catch (error) {
       res.send(
         {
-          status:500,
+          status: 500,
           message: 'Internal server error'
         }
       );
--- src/ministeroffice/ministeroffice.service.ts
+++ src/ministeroffice/ministeroffice.service.ts
@@ -9,16 +9,85 @@ export class MinistryOfficeService {
   constructor(
     @InjectModel(OwnerDetails.name)
     private ownerDetailsModel: Model<OwnerDetails>,
-     @InjectModel(OwnerRelations.name)
+    @InjectModel(OwnerRelations.name)
     private ownerRelationsModel: Model<OwnerRelations>,
-  ) {}
+  ) { }
+
+  async saveOwnerDetails2(ownerData: OwnerDetailsDTO): Promise<OwnerDetails> {
+    // Check if the OwnerDetails already exist
+    let existingOwnerDetails = await this.ownerDetailsModel.findOne({ OwnerCivilIdNumber: ownerData.OwnerCivilIdNumber });
+
+    if (!existingOwnerDetails) {
+      // If OwnerDetails do not exist, create new OwnerDetails
+      existingOwnerDetails = new this.ownerDetailsModel(ownerData);
+      await existingOwnerDetails.save();
+
+      // Save the relations if they exist in the DTO
+      if (ownerData.Relations && ownerData.Relations.length > 0) {
+        const relationDocs = ownerData.Relations.map(relation => ({
+          ...relation,
+          OwnerCivilIdNumber: ownerData.OwnerCivilIdNumber,
+        }));
+        await this.ownerRelationsModel.insertMany(relationDocs);
+      }
+    } else {
+      existingOwnerDetails.Notes = ownerData.Notes;
+      existingOwnerDetails.OwnerFullName = ownerData.OwnerFullName;
+      existingOwnerDetails.OwnerOccupation = ownerData.OwnerOccupation;
+      // If OwnerDetails exist, check and update the relations
+      if (ownerData.Relations && ownerData.Relations.length > 0) {
+        for (const newRelation of ownerData.Relations) {
+          const existingRelation = await this.ownerRelationsModel.findOne({
+            OwnerCivilIdNumber: ownerData.OwnerCivilIdNumber,
+            RelatedCivilIdNumber: newRelation.RelatedCivilIdNumber,
+          });
+
+          if (existingRelation) {
+            // Update the existing relation
+            existingRelation.Notes = newRelation.Notes;
+            await existingRelation.save();
+          } else {
+            // Add new relation
+            const relationDoc = new this.ownerRelationsModel({
+              ...newRelation,
+              OwnerCivilIdNumber: ownerData.OwnerCivilIdNumber,
+            });
+            await relationDoc.save();
+
+            // Update the relations reference in OwnerDetails
+            // existingOwnerDetails.Relations.push(relationDoc._id);
+          }
+        }
+
+        await existingOwnerDetails.save();
+      }
+    }
+
+    return existingOwnerDetails;
+  }
+
+  async getOwnerDetails(ownerCivilIdNumber: string): Promise<any> {
+    try {
+        const ownerRelations = await this.ownerRelationsModel.find({ OwnerCivilIdNumber: ownerCivilIdNumber }).exec();
+        //const ownerDetails = await this.ownerDetailsModel.findOne({ OwnerCivilIdNumber: ownerCivilIdNumber }).exec();
+
+        return {  ownerRelations };
+    } catch (error) {
+        console.error("Error fetching owner details:", error);
+        throw error;
+    }
+}
+
+
+
+
 
   async saveOwnerDetails(
     ownerData: OwnerDetailsDTO,
   ): Promise<OwnerDetails> {
     console.log(ownerData)
     const createdMinistryOffice = new this.ownerDetailsModel(ownerData).save()
-    return  await createdMinistryOffice
+    return await createdMinistryOffice
   }
 
   async addRelation(ownerCivilIdNumber: string, updateRelationDto: OwnerRelationsDTO): Promise<OwnerDetails> {
@@ -32,7 +101,7 @@ export class MinistryOfficeService {
 
   async updateOwner(ownerCivilIdNumber: string, updateUserDto: OwnerDetailsDTO): Promise<OwnerDetails> {
     const { OwnerCivilIdNumber, Relations, ...updateFields } = updateUserDto;
-   return await this.ownerDetailsModel.findOneAndUpdate(
+    return await this.ownerDetailsModel.findOneAndUpdate(
       { OwnerCivilIdNumber: ownerCivilIdNumber },
       { $set: updateFields },
       { new: true, useFindAndModify: false }
@@ -40,54 +109,54 @@ export class MinistryOfficeService {
   }
 
 
-  
-async findbyNoteCivilId(civilId: string, page: number, limit: number): Promise<{
+
+  async findbyNoteCivilId(civilId: string, page: number, limit: number): Promise<{
     data: OwnerDetails[],
     totalItems: number,
     currentpage: number,
-    totalpages:number
+    totalpages: number
   }> {
     const totalCount = await this.ownerDetailsModel.countDocuments({
-      civilIdNumber:civilId
+      civilIdNumber: civilId
     });
-    
+
     const totalPages = Math.ceil(totalCount / limit);
     const results = await this.ownerDetailsModel
-        .find({ civilIdNumber: civilId })
-        .select('-_id -__v')
-        .skip((page - 1) * limit)
-        .limit(limit)
-        .lean()
-        .exec();
-    return {  
+      .find({ civilIdNumber: civilId })
+      .select('-_id -__v')
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean()
+      .exec();
+    return {
       currentpage: page,
-      totalpages:totalPages,
+      totalpages: totalPages,
       totalItems: totalCount,
       data: results,
     };
-}
- 
+  }
+
   async findbyAllNotes(page: number, limit: number): Promise<{
     data: OwnerDetails[],
     totalItems: number,
     currentpage: number,
-    totalpages:number
+    totalpages: number
   }> {
     const totalCount = await this.ownerDetailsModel.countDocuments();
     const totalPages = Math.ceil(totalCount / limit);
     const results = await this.ownerDetailsModel
-        .find()
-        .select('-_id -__v')
-        .skip((page - 1) * limit)
-        .limit(limit)
-        .lean()
-        .exec();
-    return {  
+      .find()
+      .select('-_id -__v')
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean()
+      .exec();
+    return {
       currentpage: page,
-      totalpages:totalPages,
+      totalpages: totalPages,
       totalItems: totalCount,
       data: results,
     };
-}
+  }
 
 }
